3b47fd8893c15b74d826142c8eb2092ecd70f511
신동훈 <easthoon1996@gmail.com>
2025-06-13 14:47:08 +0900

    - 최초 기동 시 서비스 변경 - 필요없는 컨트롤러, 서비스 제거

--- src/main/java/com/dreamsecurity/sapmock/component/StartupEmployeeGenerator.java
+++ src/main/java/com/dreamsecurity/sapmock/component/StartupEmployeeGenerator.java
@@ -1,6 +1,7 @@
 package com.dreamsecurity.sapmock.component;
 
-import com.dreamsecurity.sapmock.service.FakeSapService;
+import com.dreamsecurity.sapmock.service.EmployeeGenerationService;
+import com.dreamsecurity.sapmock.service.EmployeeQueryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,24 +14,26 @@ public class StartupEmployeeGenerator implements ApplicationRunner {
 
     private static final Logger log = LoggerFactory.getLogger(StartupEmployeeGenerator.class);
 
-    private final FakeSapService employeeService;
+    private final EmployeeGenerationService employeeGenerationServiceService;
+    private final EmployeeQueryService employeeQueryService;
 
     @Value("${sap.gen.count:50}") // 기본값 50
     private int defaultCount;
 
-    public StartupEmployeeGenerator(FakeSapService employeeService) {
-        this.employeeService = employeeService;
+    public StartupEmployeeGenerator(EmployeeGenerationService employeeGenerationServiceService, EmployeeQueryService employeeQueryService) {
+        this.employeeGenerationServiceService = employeeGenerationServiceService;
+        this.employeeQueryService = employeeQueryService;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (employeeService.hasEnoughEmployees(defaultCount)) {
+        if (employeeQueryService.hasEnoughEmployees(defaultCount)) {
             log.info("기존에 직원이 {}명 이상 존재하므로 생성 생략", defaultCount);
             return;
         }
 
         log.info("SAP Mock 서버 기동 후 직원 {}명 생성 시작", defaultCount);
-        employeeService.generateEmployees(defaultCount);
+        employeeGenerationServiceService.generateEmployees(defaultCount);
         log.info("초기 직원 데이터 생성 완료!");
     }
 }
